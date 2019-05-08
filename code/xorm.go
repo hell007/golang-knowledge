40ced7d183f5cb993f2ef8074d824620c5df7b64
hell007 <1048523306@qq.com>
2019-05-08 11:59:06 +0800

    update

--- code/xorm.go
+++ code/xorm.go
@@ -11,7 +11,7 @@ package main
 import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	//"github.com/go-xorm/core"
+	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
 	"log"
 	"time"
@@ -19,10 +19,10 @@ import (
 
 //定义结构体(xorm支持双向映射)
 type Userinfo struct {
-	uid        int       `xorm:"int(10) pk autoincr"` //指定主键并自增
-	username   string    `xorm:"varchar(64) unique"`  //唯一的
-	department string    `xorm:"varchar(64)"`
-	created    time.Time `xorm:"created"`
+	Uid        int       `xorm:"int(10) pk autoincr"` //指定主键并自增
+	Username   string    `xorm:"varchar(64) unique"`  //唯一的
+	Department string    `xorm:"varchar(64)"`
+	Created    time.Time `xorm:"created"`
 	//Version    int     `xorm:"version"` //乐观锁
 }
 
@@ -43,17 +43,12 @@ func init() {
 }
 
 //增
-func Insert(username string, department string, created time.Time) (string, string, time.Time) {
-	u1 := new(Userinfo)
-	u1.username = "曹操"
-	u1.department = "研发部"
-	u1.created = time.Now()
-
-	affected, err := x.Insert(u1)
+func Insert(userinfo *Userinfo) {
+	affected, err := x.Insert(userinfo)
 	if err != nil {
-		return affected, false
+		log.Fatal("添加失败:", err)
 	}
-	return affected, true
+	fmt.Println(affected)
 }
 
 //删
@@ -76,12 +71,11 @@ func update(uid int, userinfo *Userinfo) bool {
 
 //查
 func getinfo(uid int) *Userinfo {
-	userinfo := &Userinfo{uid: uid}
+	userinfo := &Userinfo{Uid: uid}
 	is, _ := x.Get(userinfo)
 	if !is {
 		log.Fatal("搜索结果不存在!")
 	}
-	fmt.println(userinfo)
 	return userinfo
 }
 
@@ -89,11 +83,51 @@ func main() {
 
 	x.ShowSQL(true) // 显示SQL的执行, 便于调试分析
 
-	//设置表名下划线转驼峰
-	//x.SetMapper(core.SameMapper{})
+	x.SetMapper(core.SameMapper{}) // 设置表名下划线转驼峰
 
-	init()
+	x.Sync2(new(Userinfo))
 
-	getinfo(8)
+	// 1.查询
+	fmt.Println("\n struct查询")
+	info := getinfo(8)
+	fmt.Println("select = ", info, "\n")
+
+	//var username string
+	id := 8
+	is1, _ := x.Where("uid = ?", 8).Cols("username").Get(&id)
+	fmt.Println("查询成功 is1= ", is1, "\n")
+
+	//
+	uid := 8
+	is2, _ := x.SQL("select username from userinfo").Get(&uid)
+	fmt.Println("查询成功 is2=", is2, "\n")
+
+	data1, _ := x.Query("select * from userinfo")
+	fmt.Println(" data1 = ", data1, "\n")
+
+	data2, _ := x.QueryString("select * from userinfo")
+	fmt.Println(" data2 = ", data2, "\n")
+
+	// 2.添加
+	fmt.Println("\n添加")
+
+	// u1 := new(Userinfo)
+	// u1.Username = "曹丕"
+	// u1.Department = "财务部"
+	// u1.Created = time.Now()
+	//Insert(u1)
+
+	// 3.更改
+	fmt.Println("\n更改")
+
+	// u4 := new(Userinfo)
+	// u4.Username = "刘小备1"
+	// u4.Department = "刘家桥"
+	// update(9, u4)
+
+	// 4.删除
+	fmt.Println("\n删除")
+
+	// Del(10)
 
 }
