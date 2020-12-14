cb43018957db694553fdaaaa11c5adb55e3dff29
hell007 <1048523306@qq.com>
2020-12-14 14:36:10 +0800

    channel的温习

--- Go-Notes-Code/Basic/goroutine_select.go
+++ Go-Notes-Code/Basic/goroutine_select.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// select用于在多个channel上同时进行侦听并收发消息，当任何一个case满足条件时即执行;
+// 如果没有可执行的case则会执行default的case;
+// 如果没有指定default，则会阻塞程序.
+
 func fibonacci2(c, quit chan int) {
 	fmt.Println("fibonacci2")
 	x, y := 1, 1
